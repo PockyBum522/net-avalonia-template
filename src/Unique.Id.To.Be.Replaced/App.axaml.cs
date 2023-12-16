914e9ac8cdf524152e1a2435e01778a86331c9ca
DSikes <david@sikespc.com>
2023-12-16 08:38:02 -0500

    Added logger and setup for logger

--- src/Unique.Id.To.Be.Replaced/App.axaml.cs
+++ src/Unique.Id.To.Be.Replaced/App.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Unique.Id.To.Be.Replaced.ApplicationLogistics;
 using Unique.Id.To.Be.Replaced.ViewModels;
 using Unique.Id.To.Be.Replaced.Views;
 
@@ -15,18 +16,24 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
+        // Dependency Injection:
+        var loggerConfiguration = LoggerSetup.ConfigureLogger();
+
+        // ReSharper disable once ConvertIfStatementToSwitchStatement because I don't know that it's actually cleaner
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainViewModel()
+                DataContext = new MainViewModel(
+                    loggerConfiguration)
             };
         }
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
         {
             singleViewPlatform.MainView = new MainView
             {
-                DataContext = new MainViewModel()
+                DataContext = new MainViewModel(
+                    loggerConfiguration)
             };
         }
 
--- src/Unique.Id.To.Be.Replaced/ViewModels/MainViewModel.cs
+++ src/Unique.Id.To.Be.Replaced/ViewModels/MainViewModel.cs
@@ -3,4 +3,19 @@
 public class MainViewModel : ViewModelBase
 {
     public string Greeting => "Welcome to Avalonia!";
+    
+    private readonly ILogger _logger;
+    private readonly LoggerConfiguration _loggerConfiguration;
+
+    public MainViewModel() { throw new NotImplementedException(); }     // This method being present keeps the designer happy
+    
+    public MainViewModel(LoggerConfiguration loggerConfiguration)
+    {
+        _loggerConfiguration = loggerConfiguration;
+
+        _logger = loggerConfiguration
+            .MinimumLevel.Debug()
+            .CreateLogger();
+    }
+
 }
